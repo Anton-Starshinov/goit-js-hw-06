00131bd10361e3472024b7f3c2658b1a64c7f71c
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-07-14 21:54:40 +0300

    Dz 6 task 3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,23 @@
-const navEl = document.querySelectorAll(".item");
-console.log(navEl);
+const numberOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", numberOfCategories.length);
+
+const categoryAnimals = numberOfCategories[0].firstElementChild.textContent;
+console.log("Category:", categoryAnimals);
+
+const elementsAnimals = numberOfCategories[0].lastElementChild.children.length;
+console.log("Elements:", elementsAnimals);
+
+const categoryProducts = numberOfCategories[1].firstElementChild.textContent;
+console.log("Category:", categoryProducts);
+
+const elementsAProducts =
+  numberOfCategories[1].lastElementChild.children.length;
+console.log("Elements:", elementsAProducts);
+
+const categoryTechnologies =
+  numberOfCategories[2].firstElementChild.textContent;
+console.log("Category:", categoryTechnologies);
+
+const elementsTechnologies =
+  numberOfCategories[2].lastElementChild.children.length;
+console.log("Elements:", elementsTechnologies);
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const newItemEl = ingredients.map((ingredient) => {
+  const navItemEl = document.createElement("li");
+  navItemEl.classList.add("item");
+  navItemEl.textContent = `${ingredient}`;
+
+  return navItemEl;
+});
+
+const elementList = document.querySelector("ul");
+elementList.append(...newItemEl);
