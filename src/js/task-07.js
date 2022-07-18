fa69f628e470da1bc17cdbe5c5da38b4a16a09a4
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-07-18 20:01:48 +0300

    dz6 task 8

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,1 +1,11 @@
-const 
+const slider = document.querySelector("#font-size-control");
+console.log(slider);
+
+const textEl = document.querySelector("#text");
+console.log(textEl);
+
+function fontControl(event) {
+  console.log(slider.value);
+  textEl.style.fontSize = `${slider.value}px`;
+}
+slider.addEventListener("input", fontControl);
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,10 @@
+const form = document.querySelector(".login-form");
+console.log(form);
+form.addEventListener("sumbit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new formData(event.currentTarget);
+  console.log(formData);
+}
