0a57d289240ec2f1553712400b8e9a5bce036952
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-07-19 16:00:20 +0300

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  document.body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`;
+
+  spanEl.textContent = document.body.style.backgroundColor;
 }
